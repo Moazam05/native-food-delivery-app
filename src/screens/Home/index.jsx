b5d82cfb4039cd61c581b2423838d16b74fe6f4c
Moazam05 <salmanmoazam08@gmail.com>
2024-10-03 18:13:51 +0500

    access current location

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,7 +1,58 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, PermissionsAndroid} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import Geolocation from 'react-native-geolocation-service';
 
 const Home = () => {
+  const [permissionGranted, setPermissionGranted] = useState(false);
+
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Foodies wants to access your location',
+          message:
+            'Foodies App needs access to your location' +
+            'so you can order food from your favorite restaurants.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        setPermissionGranted(true);
+        console.log('You can use the camera');
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const getCurrentLocation = () => {
+    if (permissionGranted) {
+      Geolocation.getCurrentPosition(
+        position => {
+          console.log(position);
+        },
+        error => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+    }
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, [permissionGranted]);
+
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
+
   return (
     <View>
       <Text>Home</Text>
