82e0e43023b8ced7d3cf69c39d99548f9097bcbc
Moazam05 <salmanmoazam08@gmail.com>
2024-10-03 23:30:14 +0500

    Products Data

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import {Product1, Product2, Product3, Product4} from '../assets/images';
+
 export const onboarding = [
   {
     id: 1,
@@ -16,5 +18,40 @@ export const onboarding = [
     title: 'We serve incomparable delicacies',
     description:
       'All the best restaurants with their top menu waiting for you, they cant’t wait for your order!!',
+  },
+];
+
+export const ProductsData = [
+  {
+    id: 1,
+    name: 'Ordinary Burger',
+    image: Product1,
+    rating: 4.5,
+    price: 499,
+    distance: 5 - 10,
+  },
+  {
+    id: 2,
+    name: 'Burger With Meat',
+    image: Product2,
+    rating: 4.3,
+    price: 650,
+    distance: 15 - 120,
+  },
+  {
+    id: 3,
+    name: 'Burger Witt Fries',
+    image: Product3,
+    rating: 4.8,
+    price: 799,
+    distance: 20 - 25,
+  },
+  {
+    id: 4,
+    name: 'Tower Burger',
+    image: Product4,
+    rating: 4.7,
+    price: 999,
+    distance: 10 - 15,
   },
 ];
