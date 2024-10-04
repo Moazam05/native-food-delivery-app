df38040ae3a94456508735468d96c6f9b93dab11
Moazam05 <salmanmoazam08@gmail.com>
2024-10-04 15:59:15 +0500

    product images showing

--- src/screens/Home/components/ProductDetail.jsx
+++ src/screens/Home/components/ProductDetail.jsx
@@ -6,13 +6,20 @@ import {
   TouchableOpacity,
   Image,
 } from 'react-native';
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import {useDispatch} from 'react-redux';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {themeColors} from '../../../constants/colors';
-import {Back} from '../../../assets/images';
+import {Back, Wishlist, WishlistFill} from '../../../assets/images';
 import {Fonts} from '../../../constants/fonts';
+import Swiper from 'react-native-swiper';
+import {imagesData} from '../../../constants';
+import useTypedSelector from '../../../hooks/useTypedSelector';
+import {
+  selectWishlistProducts,
+  setWishListProducts,
+} from '../../../redux/wishlist/wishlistsSlice';
 
 const ProductDetail = () => {
   const navigation = useNavigation();
@@ -20,8 +27,31 @@ const ProductDetail = () => {
   const dispatch = useDispatch();
   const route = useRoute();
   const {item} = route.params || {};
+  const wishListProducts = useTypedSelector(selectWishlistProducts);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [productImages, setProductImages] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
 
   console.log('item', item);
+  console.log('productImages', productImages);
+
+  useEffect(() => {
+    const findImages = imagesData.find(pr => pr.linkId === item.linkId);
+    if (findImages) {
+      setProductImages(findImages.images);
+    }
+
+    const findFavorite = wishListProducts.some(
+      product => product.id === item.id,
+    );
+    setIsFavorite(findFavorite);
+  }, [item]);
+
+  const toggleFavorite = () => {
+    setIsFavorite(prev => !prev);
+    dispatch(setWishListProducts(item));
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -41,6 +71,34 @@ const ProductDetail = () => {
         <Text style={styles.title}>About This Menu</Text>
         <Text style={styles.h}>Hide</Text>
       </View>
+
+      <View
+        style={{
+          height: 300,
+          justifyContent: 'center',
+          width: '100%',
+          position: 'relative',
+          marginTop: 10,
+        }}>
+        <Swiper
+          ref={swiperRef}
+          loop={false}
+          onIndexChanged={index => setActiveIndex(index)}
+          showsPagination={false}>
+          {productImages.map((image, index) => (
+            <View key={index} style={styles.banner}>
+              <Image source={image} style={styles.bannerImg} />
+            </View>
+          ))}
+        </Swiper>
+
+        <TouchableOpacity style={styles.wishWrap} onPress={toggleFavorite}>
+          <Image
+            source={isFavorite ? WishlistFill : Wishlist}
+            style={styles.wishlistIcon}
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
@@ -57,7 +115,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginHorizontal: 20,
-    marginVertical: 20,
+    marginTop: 20,
   },
   backIcon: {
     width: 30,
@@ -71,5 +129,30 @@ const styles = StyleSheet.create({
   },
   h: {
     opacity: 0,
+  },
+  banner: {
+    marginHorizontal: 16,
+    marginVertical: 16,
+    position: 'relative',
+  },
+  bannerImg: {
+    borderRadius: 12,
+    width: '100%',
+    height: 250,
+    resizeMode: 'cover',
+  },
+  wishWrap: {
+    position: 'absolute',
+    top: 25,
+    right: 25,
+    backgroundColor: '#F9F9F9',
+    padding: 8,
+    borderRadius: 50,
+  },
+  wishlistIcon: {
+    width: 16,
+    height: 16,
+    resizeMode: 'contain',
+    tintColor: themeColors.ERROR,
   },
 });
