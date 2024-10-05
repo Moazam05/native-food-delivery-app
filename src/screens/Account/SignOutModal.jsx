a3a5a6bf882b1f3b9de55562d1b3f3c1216646df
Moazam05 <salmanmoazam08@gmail.com>
2024-10-05 14:53:39 +0500

    New files created

---
+++ src/screens/Account/SignOutModal.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const SignOutModal = () => {
+  return (
+    <View>
+      <Text>SignOutModal</Text>
+    </View>
+  );
+};
+
+export default SignOutModal;
